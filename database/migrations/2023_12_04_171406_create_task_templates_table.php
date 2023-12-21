0ae8bae054b0b4aff879b8ddcba8d2b3c7c5595a
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-12-21 06:35:23 +0630

    schema fix

--- database/migrations/2023_12_04_171406_create_task_templates_table.php
+++ database/migrations/2023_12_04_171406_create_task_templates_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('task_templates', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('service_template_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
