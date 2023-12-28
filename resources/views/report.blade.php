53d9d93c645eba096fabf30c90d87afe7dfa9cb5
Pyae Sone Win <pyaesone.win@wavemoney.com.mm>
2023-12-28 14:19:37 +0630

    storage updated

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -20,11 +20,13 @@
 </head>
 <body class="bg-white p-8">
 <div class="max-w-4xl mx-auto p-6 mb-4">
-    <img src="/report_banner.jpeg" alt="Service Image" class="rounded my-8">
+    <img src="/storage/report_banner.jpeg" alt="Service Image" class="rounded my-8">
     <div class="flex justify-between items-center mb-2">
         <div class="flex gap-2">
             <div class="text-xs uppercase text-gray-600">Service Report</div>
-            <div class="text-xs font-bold">{{ $service->service_date }} | {{ $service->service_time }}</div>
+            <div class="text-xs font-bold">
+                {{ $service->service_number }} | {{ $service->service_date }} | {{ $service->service_time }}
+            </div>
         </div>
         <div class="bg-blue-100 text-blue-800 text-sm font-medium px-2.5 py-0.5 rounded">AdHoc</div>
     </div>
