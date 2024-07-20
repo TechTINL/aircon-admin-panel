d44746b5c03c263d0e3fe338f2d4c9f2d1410e66
your-usernamegit <hamzakhan286692@gmail.com>
2024-07-20 17:05:21 +0500

    working on task table

--- resources/js/Components/Services/TimelineTable.jsx
+++ resources/js/Components/Services/TimelineTable.jsx
@@ -1,18 +1,74 @@
 import { useEffect, useRef, useState } from 'react';
-// import { CalendarIcon, CaretDown } from "../../components/svgs";
-// import { CustomizedCalendar } from "./CustomizedCalendar";
 import { formatTime } from '../../Helpers/utils';
 import JobDetailsModal from './JobDetailsModal';
 import EditServiceReportModal from './EditServiceReportModal';
 
 export const TimelineTable = ({ data, setIsShowLoading }) => {
     const [appointments, setAppointments] = useState([]);
-    const eventsRef = useRef < HTMLDivElement > null;
+    const eventsRef = useRef<HTMLDivElement>(null);
     const [scrollLeft, setScrollLeft] = useState(0);
     const [openModal, setOpenModal] = useState(false);
     const [openEditModal, setOpenEditModal] = useState(false);
     const [tasks, setTasks] = useState([]);
-    const [selectedService, setSelectedService] = useState(null)
+    const [selectedService, setSelectedService] = useState(null);
+
+    const initialData = [
+        {
+            id: 1,
+            name: "Unassigned",
+            assignStatus: "Assign Now",
+            address: "Blk 129 Bt Batok, #12-60, Singapore 3...",
+            contractName: "Contract Name",
+            progress: "3 of 4"
+        },
+        {
+            id: 2,
+            name: "Unassigned",
+            assignStatus: "Assign Now",
+            address: "Blk test 129 Bt Batok, #12-60, Singapore 3...",
+            contractName: "Contract Name",
+            progress: "3 of 4"
+        },
+        {
+            id: 4,
+            name: "Unassigned",
+            assignStatus: "Assign Now",
+            address: "Blk test 129 Bt Batok, #12-60, Singapore 3...",
+            contractName: "Contract Name",
+            progress: "3 of 4"
+        },
+        {
+            id: 5,
+            name: "Unassigned",
+            assignStatus: "Assign Now",
+            address: "Blk test 129 Bt Batok, #12-60, Singapore 3...",
+            contractName: "Contract Name",
+            progress: "3 of 4"
+        },
+        {
+            id: 8,
+            name: "Unassigned",
+            assignStatus: "Assign Now",
+            address: "Blk test 129 Bt Batok, #12-60, Singapore 3...",
+            contractName: "Contract Name",
+            progress: "3 of 4"
+        },
+       
+    ];
+
+    const employeesData = [
+        { id: 1, name: "John Doe" },
+        { id: 2, name: "Jane Smith" },
+        { id: 3, name: "Alice Johnson" },
+        { id: 4, name: "Michael Brown" },
+        { id: 5, name: "Emily Davis" },
+        { id: 6, name: "Christopher Wilson" },
+        { id: 7, name: "Jessica Taylor" },
+        { id: 8, name: "David Anderson" },
+        { id: 9, name: "Sarah Martinez" },
+        { id: 10, name: "James White" }
+    ];
+    
 
     const handleEdit = () => {
         setOpenModal(false);
@@ -23,7 +79,7 @@ export const TimelineTable = ({ data, setIsShowLoading }) => {
         const columns = [];
         for (let i = 8; i <= 20; i++) {
             columns.push(
-                <th className="py-2 min-w-[100px] border border-[#BCBDC0]">
+                <th className="py-2 min-w-[100px] border border-[#BCBDC0]" key={`hour-${i}`}>
                     {isHeader && (
                         <span className="text-[12px] text-black">
                             {formatTime(`${i}:00`)}
@@ -33,7 +89,7 @@ export const TimelineTable = ({ data, setIsShowLoading }) => {
             );
             if (i < 20) {
                 columns.push(
-                    <th className="py-2 min-w-[100px] max-w-[100px] border border-[#BCBDC0]">
+                    <th className="py-2 min-w-[100px] max-w-[100px] border border-[#BCBDC0]" key={`half-hour-${i}`}>
                         {isHeader && (
                             <span className="text-[12px] text-black">
                                 {formatTime(`${i}:30`)}
@@ -51,9 +107,7 @@ export const TimelineTable = ({ data, setIsShowLoading }) => {
         let [startHour, startMin] = time?.split(':');
         startHour = Number(startHour) + (indicator?.toUpperCase() === 'PM' ? 12 : 0);
         const startTime = (Number(startHour) - 8) * 60 + Number(startMin);
-
         const endTime = startTime + 60;
-
         const left = 0 + 100 * (startTime / 30);
         const width = 100 + 100 * ((endTime - startTime) / 30);
 
@@ -101,62 +155,26 @@ export const TimelineTable = ({ data, setIsShowLoading }) => {
         };
     };
 
-    const renderTaskCard = (task, i) => {
-        const styles = getTaskClassNames(task, i);
-        return (
-            <td
-                key={`j-${i}`}
-                onClick={() => {
-                    setSelectedService(task);
-                    setOpenModal(true);
-                }}
-                className={`absolute top-[4%] h-[90%] w-[95%] flex rounded-r-2xl cursor-pointer overflow-ellipsis hover: z-[1000]`}
-                style={{
-                    backgroundColor: styles.backgroundColor,
-                    left: styles.left,
-                    width: styles.width,
-                    zIndex: styles.zIndex
-                }}
-            >
-                <div
-                    className={`rounded-lg h-full w-2`}
-                    style={{ backgroundColor: styles.tileBgColor }}
-                ></div>
-                <div className="flex flex-col p-2">
-                    <span
-                        className={`font-bold flex flex-nowrap`}
-                        style={{ color: styles.titleTextColor }}
-                    >
-                        <span className='max-w-[140px] whitespace-nowrap overflow-hidden text-ellipsis'>
-                            {task?.name || 'N/A'}
-                        </span>
-                        <span
-                            className="italic text-[12px] font-thin pl-2"
-                            style={{ color: styles.statusTextColor }}
-                        >
-                            {task.status}
-                        </span>
-                    </span>
-                    <span className={`text-[10px]`} style={{ color: styles.textColor }}>
-                        {task?.service_address}
-                    </span>
-                </div>
-            </td>
-        );
-    };
-
     useEffect(() => {
         if (data?.length > 0) {
             setTasks(data);
         }
-    }, [data])
-
+    }, [data]);
 
     useEffect(() => {
         if (eventsRef?.current) {
             eventsRef?.current.scroll(scrollLeft, 0);
         }
     }, [eventsRef, scrollLeft]);
+
+    const hours = Array.from({ length: 18 }, (_, i) => i + 8);
+
+    const formatHour = (hour) => {
+        const isPM = hour >= 12;
+        const displayHour = hour % 12 || 12; // Convert to 12-hour format
+        const period = isPM ? 'PM' : 'AM';
+        return `${displayHour}:00 ${period}`;
+    };
 
     return (
         <div className="w-full h-[80vh] overflow-x-auto overflow-y-auto rounded-xl">
@@ -171,115 +189,95 @@ export const TimelineTable = ({ data, setIsShowLoading }) => {
                 setOpenModal={setOpenEditModal}
             />
             <table className="min-w-full bg-table-header-bg text-black relative">
-                <thead className="relative">
+                <thead className="bg-gray-200">
                     <tr>
                         <th
-                            className="px-4 py-2 sticky left-0 min-w-[150px] bg-table-header-bg"
+                            className="px-4 py-2 sticky top-0 left-0 bg-gray-200 border border-gray-300"
                             rowSpan={2}
                         >
                             Time
                         </th>
-                      
-                        
-                        <tr>
-                            <th
-                                className="px-4 py-2 sticky top-0 h-10 overflow-hidden border border-[#BCBDC0] border-t-0"
-                                colSpan={25}
-                            ></th>
-                        </tr>
-                        <tr className="h-10">{...renderColumns(true)}</tr>
-                        {/* Add table header columns for other days */}
+                        <th
+                            className="px-4 py-2 sticky text-sm text-nowrap top-0 h-10 overflow-hidden border border-[#BCBDC0] border-t-0"
+                            colSpan={24} // Adjusted colSpan to match the number of hours
+                        >
+                            <div className="flex">
+                                {hours.map(hour => (
+                                    <div key={hour} className="p-2 border font-sm rounded m-1 bg-gray-100 text-center w-20">
+                                        {formatHour(hour)}
+                                    </div>
+                                ))}
+                            </div>
+                        </th>
                     </tr>
                 </thead>
                 <tbody className="relative bg-white">
-                <tr>
-                            <td className="px-4 py-2 sticky left-0 bg-[#F8F8F8] border border-[#F2F2F2] z-40">
-                                UnAssigned
+                    {/* Unassigned Tasks Row */}
+                    <tr>
+                        <td className="px-4 py-2 sticky left-0 bg-[#F8F8F8] border border-[#F2F2F2] z-40">
+                            Unassigned
+                        </td>
+                        {initialData.map((data) => (
+                            <td key={data.id} className="border border-[#BCBDC0]">
+                                <div className='bg-[#F8F8F8] py-3 rounded mx-2 border-l-8 border-custom-border'>
+                                    <div className='flex justify-between'>
+                                        <p className='mx-2 text-sm font-bold'>{data.name}</p>
+                                        <p className='mx-2 text-sm text-yellow'>{data.assignStatus}</p>
+                                    </div>
+                                    <p className='max-w-full px-2 text-sm overflow-hidden text-ellipsis whitespace-nowrap'>{data.address}</p>
+                                    <div className='flex justify-between mt-3'>
+                                        <p className='mx-2 text-sm font-bold'>{data.contractName}</p>
+                                        <p className="mx-2 text-sm flex items-center">
+                                            <span className="mx-0">
+                                                <svg
+                                                    stroke="currentColor"
+                                                    fill="currentColor"
+                                                    strokeWidth="0"
+                                                    viewBox="0 0 256 256"
+                                                    className="lg:mx-1"
+                                                    height="22"
+                                                    width="22"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                >
+                                                    <path d="M106,112a6,6,0,0,1,6-6h32a6,6,0,0,1,0,12H112A6,6,0,0,1,106,112ZM230,72V200a14,14,0,0,1-14,14H40a14,14,0,0,1-14-14V72A14,14,0,0,1,40,58H82V48a22,22,0,0,1,22-22h48a22,22,0,0,1,22,22V58h42A14,14,0,0,1,230,72ZM94,58h68V48a10,10,0,0,0-10-10H104A10,10,0,0,0,94,48ZM38,72v42.79A186,186,0,0,0,128,138a185.91,185.91,0,0,0,90-23.22V72a2,2,0,0,0-2-2H40A2,2,0,0,0,38,72ZM218,200V128.37A198.12,198.12,0,0,1,128,150a198.05,198.05,0,0,1-90-21.62V200a2,2,0,0,0,2,2H216A2,2,0,0,0,218,200Z"></path>
+                                                </svg>
+                                            </span>
+                                            <span className='mx-0'>3 of 4</span>
+                                        </p>
+                                    </div>
+                                </div>
                             </td>
-                            <tr className="h-[120px] relative">
-                            <div className='absolute bg-[#F8F8F8] py-3 rounded mx-12 mt-4 border-l-8 border-custom-border'>
-                                    <div className='flex justify-between'>
-                                        <p className='mx-2 text-sm font-bold'>Unassigned</p>
-                                        <p className='mx-2 text-sm text-yellow'>Assign Now</p>
-                                    </div>
-                                    <p className='max-w-full px-2 text-sm overflow-hidden text-ellipsis whitespace-nowrap'>Blk 129 Bt Batok, #12-60, Singapore 3...</p>
-                                    <div className='flex justify-between mt-3'>
-                                        <p className='mx-2 text-sm font-bold'>Contract Name</p>
+                        ))}
+                    </tr>
 
-                                        <p className="mx-2 text-sm flex items-center">
-                                            <span className="mx-0">
-                                                <svg
-                                                stroke="currentColor"
-                                                fill="currentColor"
-                                                stroke-width="0"
-                                                viewBox="0 0 256 256"
-                                                className="lg:mx-1"
-                                                height="22"
-                                                width="22"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                >
-                                                <path d="M106,112a6,6,0,0,1,6-6h32a6,6,0,0,1,0,12H112A6,6,0,0,1,106,112ZM230,72V200a14,14,0,0,1-14,14H40a14,14,0,0,1-14-14V72A14,14,0,0,1,40,58H82V48a22,22,0,0,1,22-22h48a22,22,0,0,1,22,22V58h42A14,14,0,0,1,230,72ZM94,58h68V48a10,10,0,0,0-10-10H104A10,10,0,0,0,94,48ZM38,72v42.79A186,186,0,0,0,128,138a185.91,185.91,0,0,0,90-23.22V72a2,2,0,0,0-2-2H40A2,2,0,0,0,38,72ZM218,200V128.37A198.12,198.12,0,0,1,128,150a198.05,198.05,0,0,1-90-21.62V200a2,2,0,0,0,2,2H216A2,2,0,0,0,218,200Z"></path>
-                                                </svg>
-                                            </span>
-                                            <span className='mx-0'>3 of 4</span>
-                                        </p>
-
-                                    </div>
-                                </div>
-                                <div className='absolute left-80 bg-[#F8F8F8] py-3 rounded mx-12 mt-4 border-l-8 border-custom-border'>
-                                    <div className='flex justify-between'>
-                                        <p className='mx-2 text-sm font-bold'>Unassigned</p>
-                                        <p className='mx-2 text-sm text-yellow'>Assign Now</p>
-                                    </div>
-                                    <p className='max-w-full px-2 text-sm overflow-hidden text-ellipsis whitespace-nowrap'>Blk 129 Bt Batok, #12-60, Singapore 3...</p>
-                                    <div className='flex justify-between mt-3'>
-                                        <p className='mx-2 text-sm font-bold'>Contract Name</p>
-
-                                        <p className="mx-2 text-sm flex items-center">
-                                            <span className="mx-0">
-                                                <svg
-                                                stroke="currentColor"
-                                                fill="currentColor"
-                                                stroke-width="0"
-                                                viewBox="0 0 256 256"
-                                                className="lg:mx-1"
-                                                height="22"
-                                                width="22"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                >
-                                                <path d="M106,112a6,6,0,0,1,6-6h32a6,6,0,0,1,0,12H112A6,6,0,0,1,106,112ZM230,72V200a14,14,0,0,1-14,14H40a14,14,0,0,1-14-14V72A14,14,0,0,1,40,58H82V48a22,22,0,0,1,22-22h48a22,22,0,0,1,22,22V58h42A14,14,0,0,1,230,72ZM94,58h68V48a10,10,0,0,0-10-10H104A10,10,0,0,0,94,48ZM38,72v42.79A186,186,0,0,0,128,138a185.91,185.91,0,0,0,90-23.22V72a2,2,0,0,0-2-2H40A2,2,0,0,0,38,72ZM218,200V128.37A198.12,198.12,0,0,1,128,150a198.05,198.05,0,0,1-90-21.62V200a2,2,0,0,0,2,2H216A2,2,0,0,0,218,200Z"></path>
-                                                </svg>
-                                            </span>
-                                            <span className='mx-0'>3 of 4</span>
-                                        </p>
-
-                                    </div>
-                                </div>
-
-                            {...renderColumns()}
-                            
-                            </tr>
-                            {/* Add table data for other time slots and days */}
+                    {/* Employee Rows */}
+                    {employeesData.map((employee) => (
+                        <tr key={employee.id} >
+                            <td className="px-4 py-2 sticky left-0 bg-[#F8F8F8] border border-[#F2F2F2] z-40">
+                                {employee.name}
+                            </td>
+                            {Array.from({ length: 24 }).map((_, index) => (
+                                <td key={index} className="border border-[#BCBDC0]" style={{ height: '100px' }}>
+                                    {/* Content for each hour slot can be added here */}
+                                </td>
+                            ))}
                         </tr>
+                    ))}
+
+                    {/* Task Rows */}
                     {tasks?.map((task, i) => (
-                        <tr>
-                            <td className="px-4 py-2 sticky left-0 bg-[#F8F8F8] border border-[#F2F2F2] z-40">
-                                {task.name}
+                        <tr key={i}>
+                            <td className="px-4 py-2 sticky left-0 bg-[#F8F8F8] border border-[#F2F2F2] z-40" >
+                                {/* Task details */}
                             </td>
-                            <tr className="h-[120px] relative">
-                                {task?.services?.map((tt, j) => renderTaskCard(tt, j))}
-                                {...renderColumns()}
-                            </tr>
-                            {/* Add table data for other time slots and days */}
+                            {task?.services?.map((tt, j) => renderTaskCard(tt, j))}
+                            {/* {renderColumns()} */}
                         </tr>
                     ))}
                 </tbody>
             </table>
         </div>
-        // <div className="flex flex-auto w-full">
-
-        //     {/* <TimelineTableHeader /> */}
-        // </div>
     );
 };
+
 export default TimelineTable;
--- resources/js/Pages/Services/Timeline.jsx
+++ resources/js/Pages/Services/Timeline.jsx
@@ -15,54 +15,52 @@ function Timeline({ auth, users }) {
             <div className="text-zinc-800 text-3xl font-bold leading-10">
               Job Table
             </div>
-            <div className="relative flex items-center group">
-              <button>
-                <AiOutlineInfoCircle
-                  className="text-primary ml-2"
-                  size={26}
-                  fontWeight={'bold'}
-                />
-              </button>
-              <div className="absolute top-14 left-10 mb-2 w-max opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-white text-black text-xs rounded py-2 px-4 z-50">
-  <div className="mb-2 font-bold">
-    Job Status
-  </div>
-  <div className="flex flex-wrap">
-  <button className="bg-gray-400 hover:bg-gray-600 text-gray-900 font-bold py-2 px-4 rounded-full mb-2 mr-2">
-  Unassigned
-</button>
-
-<button className="bg-hold hover:bg-blue-500 text-texthold font-bold py-2 px-4 rounded-full mb-2 mr-2">
-  On-Hold
-</button>
-
-    <button className="bg-textschedule hover:bg-green-700 text-schedule font-bold py-2 px-4 rounded-full mb-2 mr-2">
-      Scheduled
-    </button>
-    <button className="bg-primary hover:bg-yellow-700 text-textschedule font-bold py-2 px-4 rounded-full mb-2 mr-2">
-      Completed
-    </button>
-    <button className="bg-red-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full mb-2 mr-2">
-      Require Follow-up
-    </button>
-    <button className="bg-followup hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full mb-2">
-      Follow-up Completed
-    </button>
-  </div>
-</div>
-
-
-            </div>
+            <div className="relative">
+            {/* Button */}
+            <button className="relative z-10 flex items-center">
+              <AiOutlineInfoCircle
+                className="text-primary ml-2"
+                size={26}
+                fontWeight={'bold'}
+              />
+              {/* Tooltip */}
+              <div className="absolute tooltip top-10 left-20 transform -translate-x-1/2 mt-2 w-max opacity-0 group-hover:opacity-100 transition-opacity duration-300 bg-white text-black text-xs rounded py-2 px-4 z-50 invisible group-hover:visible">
+                <div className="mb-2 font-bold">
+                  Job Status
+                </div>
+                <div className="flex flex-wrap">
+                  <button className="bg-gray-400 hover:bg-gray-600 text-gray-900 font-bold py-2 px-4 rounded-full mb-2 mr-2">
+                    Unassigned
+                  </button>
+                  <button className="bg-hold hover:bg-blue-500 text-texthold font-bold py-2 px-4 rounded-full mb-2 mr-2">
+                    On-Hold
+                  </button>
+                  <button className="bg-textschedule hover:bg-green-700 text-schedule font-bold py-2 px-4 rounded-full mb-2 mr-2">
+                    Scheduled
+                  </button>
+                  <button className="bg-primary hover:bg-yellow-700 text-textschedule font-bold py-2 px-4 rounded-full mb-2 mr-2">
+                    Completed
+                  </button>
+                  <button className="bg-red-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full mb-2 mr-2">
+                    Require Follow-up
+                  </button>
+                  <button className="bg-followup hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full mb-2">
+                    Follow-up Completed
+                  </button>
+                </div>
+              </div>
+            </button>
+          </div>
           </div>
           <div className="flex items-center pr-4">
             <JobFilters />
           </div>
         </div>
         <div className="flex flex-1 mt-6 max-w-[80vw] relative">
-          <div className="absolute flex items-center top-2 right-[35vw] z-30">
-            <AiOutlineLeft fontWeight={900} />
-            <span className="text-black font-bold px-4">10 Dec 2023</span>
-            <AiOutlineRight />
+          <div className="absolute flex items-center top-0 right-[35vw] z-10">
+            {/* <AiOutlineLeft fontWeight={900} />
+            <span className="text-black font-bold px-4 date-month">10 Dec 2023</span>
+            <AiOutlineRight /> */}
           </div>
           <TimelineTable data={users} />
         </div>
